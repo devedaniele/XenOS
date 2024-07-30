44ea5c327cc72ddd1079c630f8b039133f38af17
devedaniele <sadhunanda9@gmail.com>
2024-07-30 06:25:55 +0000

    init

---
+++ termux.bash_aliases.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+alias X11="termux-x11: 1 &"
+alias xenos="pd login debian"
+alias update="apt update && apt upgrade -y"
---
+++ xenos.bash_aliases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+alias myip="curl ip-api.com"
+alias termux="cd /data/data/com.termux/files/home"
+alias update="apt update && apt upgrade -y"
